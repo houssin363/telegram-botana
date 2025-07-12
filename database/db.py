1979c8b1e997367b91e2c098fb6f64ee521d8a96
houssin363 <houssin361990@gmail.com>
2025-07-12 22:56:24 +0100

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,7 +1,11 @@
+# database/db.py
+
 from supabase import create_client, Client
-from config import SUPABASE_URL, SUPABASE_API_KEY
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_API_KEY)
+SUPABASE_URL = "https://azortroeejjomqweintc.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6b3J0cm9lZWpqb21xd2VpbnRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxOTIzNjUsImV4cCI6MjA2Nzc2ODM2NX0.x3Pwq8OyRmlr7JQuEU2xRxYJtSoz67eIVzDx8Nh4muk"
 
-def get_table(name):
-    return supabase.table(name)
+client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def get_table(table_name):
+    return client.table(table_name)
