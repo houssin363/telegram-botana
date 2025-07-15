ab95fc653e4dcd81cb981dbf8b2dfd7b8dee2a88
houssin363 <houssin361990@gmail.com>
2025-07-15 22:42:49 +0100

    Update cash_transfer.py

--- handlers/cash_transfer.py
+++ handlers/cash_transfer.py
@@ -381,8 +381,11 @@ def register(bot, history):
             ("✏️ تعديل", "edit_final_company"),
             ("✔️ تأكيد", "send_request_company")
         )
-        bot.send_message(
-            call.message.chat.id,
-            f"""🟢 هل أنت متأكد من إرسال حوالة مالية قدرها {amount:,} ل.س
-للمستلم {fullname} (رقم: {phone})؟
-🧾
+       bot.send_message(
+    call.message.chat.id,
+    f"🟢 هل أنت متأكد من إرسال حوالة مالية قدرها {amount:,} ل.س\n"
+    f"للمستلم {fullname} (رقم: {phone})؟\n"
+    f"🧾 العمولة: {commission:,} ل.س\n"
+    f"✅ الإجمالي: {total:,} ل.س",
+    reply_markup=kb
+    )
