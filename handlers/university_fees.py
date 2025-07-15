9e04105feae513697733d9f45ede91cb8e856be2
houssin363 <houssin361990@gmail.com>
2025-07-15 17:29:40 +0100

    Update university_fees.py

--- handlers/university_fees.py
+++ handlers/university_fees.py
@@ -1,6 +1,6 @@
 # handlers/university_fees.py
 from telebot import types
-from handlers.wallet_service import has_sufficient_balance, deduct_balance
+from services.wallet_service import has_sufficient_balance, deduct_balance
 from config import ADMIN_MAIN_ID
 
 COMMISSION_PER_50000 = 3500
